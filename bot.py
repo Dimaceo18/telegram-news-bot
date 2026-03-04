09d825b62784155e643851b295c9a6e2ccd64e81
Dimaceo18 <dimaceo@gmail.com>
2026-03-05 00:09:55 +0300

    Implement Telegram bot to handle photos and text

---
+++ bot.py
@@ -0,0 +1,27 @@
+import telebot
+
+TOKEN = "ВСТАВЬ_СЮДА_НОВЫЙ_ТОКЕН"
+CHANNEL = "@unclefider"
+
+bot = telebot.TeleBot(TOKEN)
+
+last_photo = None
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    global last_photo
+    last_photo = message.photo[-1].file_id
+    bot.reply_to(message, "Фото получено. Теперь отправьте текст.")
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    global last_photo
+    
+    if last_photo:
+        bot.send_photo(CHANNEL, last_photo, caption=message.text)
+        bot.reply_to(message, "Новость опубликована.")
+        last_photo = None
+    else:
+        bot.reply_to(message, "Сначала отправьте фото.")
+
+bot.infinity_polling()
